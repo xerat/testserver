b939e1d620543f551cabb6d8bec913aa2146a3e5
Thomas Sorensen <thomas.simoni.sorensen@gmail.com>
2014-05-17 06:54:17 +1000

    hello

---
+++ test/unit/user_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+	test "a user should enter a first name" do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:first_name].empty?
+	end	
+end
